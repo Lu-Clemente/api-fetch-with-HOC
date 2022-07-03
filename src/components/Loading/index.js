114298a1a7aa0e3e267545967142038180abeb58
Luciene Clemente <luciene.clemente@unifei.edu.br>
2022-07-03 18:18:18 -0300

    Loading component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './styles.css';
+
+const Loading = () => {
+    return (
+        <div className="loading--div">
+            <h1 className="loading--text">Loading data...</h1>
+        </div>
+    )
+}
+
+export default Loading
